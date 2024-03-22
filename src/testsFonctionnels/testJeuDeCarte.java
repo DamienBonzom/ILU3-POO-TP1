659785209082a85f638b024c664032ad188e2e36
DamienBonzom <damienbonzom31@gmail.com>
2024-03-22 10:16:38 +0100

    fin tp2 avec test

--- src/testsFonctionnels/testJeuDeCarte.java
+++ src/testsFonctionnels/testJeuDeCarte.java
@@ -1,7 +1,10 @@
 package testsFonctionnels;
 
 import cartes.JeuDeCartes;
+
 import package1.Carte;
+
+import utils.Utils;
 
 public class testJeuDeCarte {
 
@@ -11,12 +14,14 @@ public class testJeuDeCarte {
 
 	public static void main(String[] args) {
 		JeuDeCartes jeu = new JeuDeCartes();
+		Utils util = new Utils();
 		
 		for(Carte carte : jeu.getListeCartes()) {
 			System.out.println(carte);
 		}
 		
 		System.out.println(String.format("Le jeu de cartes est bien construit : %b\n", jeu.checkCount()));
+		System.out.println(String.format("Jeu de carte rassemblé, verification : %b\n", util.verifierRassemblement(util.rassembler(jeu.getListeCartes()))));
 
 	}
 
